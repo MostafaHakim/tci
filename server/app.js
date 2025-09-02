04d5a4ee1c5cd3519cfac3f486292c8ae4395b6e
Md Mostafa <mdmostafahakim@gmail.com>
2025-09-02 20:10:49 +0600

    first commit

--- server/app.js
+++ server/app.js
@@ -9,10 +9,7 @@ const MONGO_URI =
   "mongodb+srv://tci:tci1234@cluster0.0cwne57.mongodb.net/tci?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGO_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
