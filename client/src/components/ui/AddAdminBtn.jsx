8420d514aa19b987022d9289f7d764b80cb39e27
MostafaHakim <mdmostafahakim@gmail.com>
2025-09-14 01:01:46 +0600

    final

--- client/src/components/ui/AddAdminBtn.jsx
+++ client/src/components/ui/AddAdminBtn.jsx
@@ -5,7 +5,7 @@ function AddAdminBtn({ setShowAddForm, adminTypeLocals }) {
     <div className="mb-6">
       <button
         onClick={() => setShowAddForm(true)}
-        className={`bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded transition ${
+        className={`bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded transition cursor-pointer ${
           adminTypeLocals !== "Main" ? "hidden" : "block"
         }`}
       >
